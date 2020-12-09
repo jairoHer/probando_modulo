87fb378b0914689a11f336b398618115052644f7
jairo <jairophg.lalala2014@gmail.com>
2020-12-09 12:43:53 -0600

    x

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include <linux/types.h>
 #include <linux/unistd.h>
 #include <linux/cred.h>
+#include <unistd.h>
+#include <sys/types.h>
 //https://cboard.cprogramming.com/linux-programming/176318-kernel-module-list-all-processes-process-information.html
 
 struct task_struct *task;        /*    Structure defined in sched.h for tasks/processes    */
